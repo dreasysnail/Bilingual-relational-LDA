dffaa7320b0dfe471b33d51890d57b11348de452
Yizhe Zhang <jeremy071242044@gmail.com>
2014-10-01 11:48:36 -0400

    Initial Commit

---
+++ GibbsLDA++/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GibbsLDA++
+//
+//  Created by Yizhe on 14-10-1.
+//  Copyright (c) 2014年 Duke. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
